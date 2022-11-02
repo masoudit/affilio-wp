7577f67eb14176f290b943371ac2d7dd80bd1e02
masoud.it <masoud.it2012@gmail.com>
2022-11-02 09:24:42 +0330

    v1.1.0 add svn

--- config.php
+++ config.php
@@ -1,6 +1,5 @@
 <?php 
-// $BASE_API = "https://blackhole.affilio.ir/api/v1";
-$BASE_API = "https://blackhole-stage.affilio.ir/api/v1";
+$BASE_API = "https://blackhole.affilio.ir/api/v1";
 if(!defined('AFFILIO_BASE_API'))
     define( 'AFFILIO_BASE_API', $BASE_API );
 
